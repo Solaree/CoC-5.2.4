44774d454beb3bec3baa97fe0dafcb6205c5ff07
Solar <115794865+Solaree@users.noreply.github.com>
2023-04-10 21:45:57 +0300

    Delete CoC directory

--- CoC/Packets/CommandFactory.py
+++
@@ -1,7 +0,0 @@
-from Packets.Commands.Client.LogicJoinAllianceCommand import *
-from Packets.Commands.Client.LogicLeaveAllianceCommand import *
-
-commands = {
-    1: LogicJoinAllanceCommand,
-    2: LogicLeaveAllanceCommand
-}
--- CoC/Packets/Commands/Server/LogicChangeAvatarNameCommand.py
+++
@@ -1,6 +0,0 @@
-from Utils.Writer import Writer
-
-class LogicChangeAvatarNameCommand(Writer):
-
-    def encode(self):
-        self.writeString("Solar")
